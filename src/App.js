c56905c4194580a65349e8c5cf4d8d826ce0fa8f
Marina Elizarova <seagirl1110@gmail.com>
2024-10-02 14:24:27 +0300

    feat: add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
+import { Form, Input, Button, Card, Typography } from 'antd';
 
 function App() {
-  return <div className="App"></div>;
+  const [dataSubmited, setDataSubmited] = useState(null);
+  const [data, setData] = useState({ name: '', description: '' });
+
+  const handleClick = (evt) => {
+    const { name, value } = evt.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setDataSubmited(data);
+    setData({ name: '', description: '' });
+  };
+
+  return (
+    <div className="App">
+      <Typography.Title>Форма с использованием Ant Design</Typography.Title>
+      <Form layout="vertical" onFinish={handleSubmit}>
+        <Form.Item label="Имя">
+          <Input
+            value={data.name}
+            onChange={handleClick}
+            name="name"
+            placeholder="Name"
+            required
+          />
+        </Form.Item>
+        <Form.Item label="Описание">
+          <Input
+            value={data.description}
+            onChange={handleClick}
+            name="description"
+            placeholder="Description"
+            required
+          />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Отправить
+        </Button>
+      </Form>
+      {dataSubmited && (
+        <Card>
+          <Typography.Title level={2}>Отправленные данные</Typography.Title>
+          <Typography.Paragraph>
+            <strong>Имя:</strong> {dataSubmited.name}
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            <strong>Описание:</strong> {dataSubmited.description}
+          </Typography.Paragraph>
+        </Card>
+      )}
+    </div>
+  );
 }
 
 export default App;
